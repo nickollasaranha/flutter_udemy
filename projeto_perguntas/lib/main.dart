48d1948916a15da212ca1272e3b7bd54c0421703
Nickollas Aranha <nickollas.aranha@gmail.com>
2023-02-12 17:48:25 -0300

    refactor: transform to list with Map

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
+import './resposta.dart';
 
 main() {
   runApp(PerguntaApp());
@@ -9,18 +11,31 @@ class _PerguntaAppState extends State<PerguntaApp> {
 
   void _responder() {
     setState(() {
-      _perguntaSelecionada++;
+      _perguntaSelecionada = (_perguntaSelecionada + 1) % 3;
     });
-
-    print(_perguntaSelecionada);
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<String> perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é seu animal favorito?'
+    final List<Map<String, Object>> perguntas = [
+      {
+        'texto': 'Qual é a sua cor favorita?',
+        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+      },
+      {
+        'texto': 'Qual é seu animal favorito?',
+        'respostas': ["Coelho", "Cobra", "Elefante", "Leão"],
+      },
+      {
+        'texto': "Qual é o seu instrutor favorito?",
+        'respostas': ["Maria", "João", "Leo", "Pedro"],
+      }
     ];
+
+    final List<Widget> respostas = [];
+    for (var textoResp in perguntas[_perguntaSelecionada].cast()['respostas']) {
+      respostas.add(Resposta(textoResp, _responder));
+    }
 
     return MaterialApp(
       home: Scaffold(
@@ -29,19 +44,8 @@ class _PerguntaAppState extends State<PerguntaApp> {
         ),
         body: Column(
           children: [
-            Text(perguntas[_perguntaSelecionada]),
-            ElevatedButton(
-              child: Text('Resposta 1'),
-              onPressed: _responder,
-            ),
-            ElevatedButton(
-              child: Text('Resposta 2'),
-              onPressed: _responder,
-            ),
-            ElevatedButton(
-              child: Text('Resposta 3'),
-              onPressed: _responder,
-            ),
+            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
+            ...respostas
           ],
         ),
       ),
